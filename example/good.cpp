46ba46a47da4e4be595642cc6cc112183a246738
Matus Chochlik <chochlik@gmail.com>
2024-02-24 09:35:10 +0100

    Added copyright headers to example C++ sources

--- example/good.cpp
+++ example/good.cpp
@@ -1,1 +1,8 @@
+/// @file
+///
+/// Copyright Matus Chochlik.
+/// Distributed under the Boost Software License, Version 1.0.
+/// See accompanying file LICENSE_1_0.txt or copy at
+/// https://www.boost.org/LICENSE_1_0.txt
+///
 int main() { return 0; }
