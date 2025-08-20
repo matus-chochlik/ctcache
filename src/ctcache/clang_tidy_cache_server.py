d9fc7a0d5ab3c794475cb6fa670981c312b0814f
Bart Louwers <bart@emeel.net>
2025-08-20 14:14:14 +0200

    keep GET for backward compatibility

--- src/ctcache/clang_tidy_cache_server.py
+++ src/ctcache/clang_tidy_cache_server.py
@@ -887,8 +887,12 @@ def ctc_cache(hashstr):
 def ctc_is_cached(hashstr):
     return "true" if clang_tidy_cache.is_cached(hashstr) else "false"
 # ------------------------------------------------------------------------------
-@ctcache_app.route("/purge_cache", methods=['DELETE'])
+@ctcache_app.route("/purge_cache", methods=['GET', 'DELETE'])
 def ctc_purge_cache():
+    # Deny GET if auth_key_writes is configured
+    # In a future update this API cannot be called with a GET request anymore
+    if flask.request.method == 'GET' and clang_tidy_cache.auth_key_writes:
+        return flask.abort(403)
     return str(clang_tidy_cache.do_purge())
 # ------------------------------------------------------------------------------
 @ctcache_app.route("/stats/cached_count")
