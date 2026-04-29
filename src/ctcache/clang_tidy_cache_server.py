matus-chochlik/ctcache

commit f3f311a8a9f5dfd08cf8b2c80a23fedcd76a6f8f

add simple authentication mechanism for writes

src/ctcache/clang_tidy_cache_server.py
@@ -150,6 +150,16 @@ class ArgumentParser(argparse.ArgumentParser):
             Starts the service in debug mode.
             """)
 
+        self.add_argument(
+            "--auth-key-writes",
+            dest="auth_key_writes",
+            metavar="STRING",
+            type=str,
+            default=None,
+            help="""
+            If specified, all non-GET requests must supply matching ?key=STRING or are rejected (403).
+            """)
+
     # -------------------------------------------------------------------------
     def process_parsed_options(self, options):
         return options
@@ -239,6 +249,13 @@ class ClangTidyCacheApp(flask.Flask):
 # ------------------------------------------------------------------------------
 clang_tidy_cache = None
 ctcache_app = ClangTidyCacheApp()
+
+@ctcache_app.before_request
+def _ctc_auth_writes():
+    if flask.request.method != 'GET':
+        if clang_tidy_cache.auth_key_writes and flask.request.args.get("key") != clang_tidy_cache.auth_key_writes:
+            flask.abort(403)
+
 # ------------------------------------------------------------------------------
 class ClangTidyCache(object):
     # --------------------------------------------------------------------------
@@ -256,6 +273,7 @@ class ClangTidyCache(object):
         self._stats_path = options.stats_path
         self._chart_path = options.chart_path
         self._max_cache_size = options.max_cache_size
+        self._auth_key_writes = options.auth_key_writes
         #
         self._info_getters = {
             "static_path": self.static_path,
@@ -527,6 +545,11 @@ class ClangTidyCache(object):
         return self._save_path
 
     # --------------------------------------------------------------------------
+    @property
+    def auth_key_writes(self):
+        return self._auth_key_writes
+
+    # --------------------------------------------------------------------------
     def save_file_size(self):
         try: return os.path.getsize(self.save_path())
         except: return 0
@@ -605,7 +628,6 @@ class ClangTidyCache(object):
         result = dict()
         for hashstr, info in self._cached.items():
             try:
-                age_days = int(round((time.time() - info["insert_time"]) / (24*3600)))
                 try:
                     result[age_days] += 1
                 except KeyError:
@@ -865,7 +887,7 @@ def ctc_cache(hashstr):
 def ctc_is_cached(hashstr):
     return "true" if clang_tidy_cache.is_cached(hashstr) else "false"
 # ------------------------------------------------------------------------------
-@ctcache_app.route("/purge_cache")
+@ctcache_app.route("/purge_cache", methods=['DELETE'])
 def ctc_purge_cache():
     return str(clang_tidy_cache.do_purge())
 # ------------------------------------------------------------------------------

src/ctcache/clang_tidy_cache.py
@@ -349,6 +349,10 @@ class ClangTidyCacheOpts:
         return int(os.getenv("CTCACHE_PORT", 5000))
 
     # --------------------------------------------------------------------------
+    def auth_key(self) -> Optional[str]:
+        return os.getenv("CTCACHE_AUTH_KEY")
+
+    # --------------------------------------------------------------------------
     def rest_host_read_only(self) -> bool:
         return getenv_boolean_flag("CTCACHE_HOST_READ_ONLY")
 
@@ -516,11 +520,16 @@ class ClangTidyServerCache:
         self.store_in_cache_with_data(digest, bytes())
 
     # --------------------------------------------------------------------------
+    def _auth_params(self):
+        ak = self._opts.auth_key()
+        return {"key": ak} if ak else {}
+
+    # --------------------------------------------------------------------------
     def store_in_cache_with_data(self, digest, data: bytes):
         if self._opts.rest_host_read_only():
             return
         try:
-            query = self._requests.put(self._make_data_url(digest), data={'data': data}, timeout=3)
+            query = self._requests.put(self._make_data_url(digest), data={'data': data}, params=self._auth_params(), timeout=3)
             if query.status_code != 200:
                 self._log.error("store_in_cache: Can't store data in server {0}, error {1}".format(
                     self._opts.rest_host(), query.status_code))

commit d6473a0a96e217252f66523cca5524a691c81bc3

Fix age_days histogram rendering
Looks like this histogram was accidentally broken in

https://github.com/matus-chochlik/ctcache/commit/f3f311a8a9f5dfd08cf8b2c80a23fedcd76a6f8f#diff-8be37f58fff41428052b2ed8c4ded1c8671e112de154936c97f8cdf71d77b19eL608

src/ctcache/clang_tidy_cache_server.py
@@ -628,6 +628,7 @@ class ClangTidyCache(object):
         result = dict()
         for hashstr, info in self._cached.items():
             try:
+                age_days = int(round((time.time() - info["insert_time"]) / (24*3600)))
                 try:
                     result[age_days] += 1
                 except KeyError: